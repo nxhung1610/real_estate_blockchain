b2017d34f7934dead248a32ff2d3db38146d7fcc
hidenxtt <nxhung16102001@gmail.com>
2022-09-14 12:31:04 +0700

    [UPDATE] - splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,19 @@ import 'package:flutter_domain_driven_design/config/app_config.dart';
 import 'package:flutter_domain_driven_design/config/app_dialog.dart';
 import 'package:flutter_domain_driven_design/config/app_notification.dart';
 import 'package:flutter_domain_driven_design/injection_dependencies/injection_dependencies.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'modules/app/presentation/app_widget.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await Future.wait([
     AppConfig.instance.initialize(),
     configureDependencies(),
     AppNotification.instance.initialize(),
   ]);
+
   runApp(const AppWidget());
+  FlutterNativeSplash.remove();
 }
