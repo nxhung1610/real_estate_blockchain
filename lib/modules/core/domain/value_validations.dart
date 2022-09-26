6102b91890ebc247cc5027a74dea7eac0bb98aaa
hidenxtt <nxhung16102001@gmail.com>
2022-09-26 13:09:45 +0700

    [REF] - validate

--- lib/modules/core/domain/value_validations.dart
+++ lib/modules/core/domain/value_validations.dart
@@ -19,3 +19,13 @@ Either<ValueFailures<String>, String> validatePassword(String input) {
     return left(ValueFailures.invalidPassword(failedValue: input));
   }
 }
+
+Either<ValueFailures<String>, String> validateFullName(String input) {
+  const emailRegex =
+      r"(?i)([0-9A-ZẮẰẲẴẶĂẤẦẨẪẬÂÁÀÃẢẠĐẾỀỂỄỆÊÉÈẺẼẸÍÌỈĨỊỐỒỔỖỘÔỚỜỞỠỢƠÓÒÕỎỌỨỪỬỮỰƯÚÙỦŨỤÝỲỶỸỴ']+\\s?\\b){2,}";
+  if (RegExp(emailRegex).hasMatch(input)) {
+    return right(input);
+  } else {
+    return left(ValueFailures.invalidEmail(failedValue: input));
+  }
+}
