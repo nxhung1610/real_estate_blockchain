99cbc3095c1f73821ab68532378a263fd2321d66
Truong Hung Huy <humghuy201280@gmail.com>
2023-02-11 12:00:15 +0700

    update - chat

--- lib/feature/core/presentation/widgets/w_custom_refresh_scroll_view.dart
+++ lib/feature/core/presentation/widgets/w_custom_refresh_scroll_view.dart
@@ -1,10 +1,6 @@
 import 'package:custom_refresh_indicator/custom_refresh_indicator.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:real_estate_blockchain/config/app_color.dart';
-import 'package:real_estate_blockchain/config/app_size.dart';
-import 'package:sliver_tools/sliver_tools.dart';
 
 class WCustomRefreshScrollView extends StatefulWidget {
   const WCustomRefreshScrollView(
@@ -41,6 +37,11 @@ class _WCustomRefreshScrollViewState extends State<WCustomRefreshScrollView>
       slivers: [
         ...widget.headers,
         CupertinoSliverRefreshControl(
+          builder: (context, refreshState, pulledExtent,
+                  refreshTriggerPullDistance, refreshIndicatorExtent) =>
+              Theme.of(context).platform == TargetPlatform.iOS
+                  ? const CupertinoActivityIndicator()
+                  : const CupertinoActivityIndicator(),
           onRefresh: widget.onRefresh,
         ),
         ...widget.children,
