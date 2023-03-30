b607b67c846a1914de4560fe8f4a6b2e6f7a6799
Truong Hung Huy <humghuy201280@gmail.com>
2023-03-31 00:19:57 +0700

    setup notification

--- lib/languages/generated/l10n.dart
+++ lib/languages/generated/l10n.dart
@@ -1,6 +1,8 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';import 'intl/messages_all.dart';
+import 'package:intl/intl.dart';
+
+import 'intl/messages_all.dart';
 
 // **************************************************************************
 // Generator: Flutter Intl IDE plugin
